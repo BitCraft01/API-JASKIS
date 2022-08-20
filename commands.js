414a660c4009e04f1c970eeb5b5f65d75d1d2fe2
Bachir <bachirmdicko@gmail.com>
2022-08-20 02:31:58 -0400

    Completed Activity

--- commands.js
+++ commands.js
@@ -1,6 +1,90 @@
 // JASKIS
 // paste the MongoDB commands you use underneath each prompt
 
+/* 
+use jaskis
+db
+db.createCollection('bounties)
+show collections
+db.bounties.insertOne({
+  name: "Thanoceros",
+  species: "Rhinoceros",
+  location: "Grasslands",
+  wantedFor: "Eating too much grass",
+  client: "Songbird",
+  reward: 10000,
+  captured: false
+})
+db.bounties.find()
+ db.bounties.insertMany([{
+    "name": "Lokinkajou",
+    "species": "Kinkajou",
+    "location": "Tropical rainforest",
+    "wantedFor": "Partying too late at night",
+    "client": "White tiger",
+    "reward": 1000,
+    "captured": false
+  },
+  {
+    "name": "Nebullama",
+    "species": "Llama",
+    "location": "Grasslands",
+    "wantedFor": "Drinking all the water from the ocean",
+    "client": "Songbird",
+    "reward": 2500,
+    "captured": false
+  },
+  {
+    "name": "Polarwind",
+    "species": "Polar Bear",
+    "location": "Arctic",
+    "wantedFor": "Not hibernating",
+    "client": "Sabertooth",
+    "reward": 4000,
+    "captured": false
+  },
+  {
+    "name": "Wrecking Crows",
+    "species": "Crow",
+    "location": "Grasslands",
+    "wantedFor": "Cawing too loudly",
+    "client": "Red wolf",
+    "reward": 40000,
+    "captured": false
+  },
+  {
+    "name": "Grandhog",
+    "species": "Groundhog",
+    "location": "Woodlands",
+    "wantedFor": "Not coming out of the hole on time and prolonging winter",
+    "client": "Songbird",
+    "reward": 50000,
+    "captured": false
+  },
+  {
+    "name": "Grim Panda",
+    "species": "Giant Panda",
+    "location": "Temperate forest",
+    "wantedFor": "Eating all the bamboo",
+    "client": "Red wolf",
+    "reward": 5000,
+    "captured": false
+  }])
+  db.bounties.find()
+  db.bounties.find({location: {$eq: 'Grasslands'}})
+  db.bounties.find({reward: {$gt: 10000}})
+  db.bounties.find({}, {client: 0})
+  db.bounties.find({$and: [{location: "Woodlands"}, {species: "Groundhog"}]})
+  db.bounties.updateOne({reward: 4000}, {$set: {reward: 10000}})
+  db.bounties.find()
+  db.bounties.deleteOne({name: "Lokinkajou"})
+  db.bounties.find()
+  db.bounties.deleteMany({client: "Songbird"})
+  db.bounties.find()
+  db.bounties.updateMany({}, {$set: {captured: "true"}})
+  db.bounties.find()
+*/
+
 // GETTING STARTED
 // 1. Create a database called jaskis
 
